33ea0597fb20deea7b0db18d521858bec2090cf1
JosephineR1985 <josephine.rathinam@gmail.com>
2019-08-07 22:13:39 -0700

    second commit

---
+++ src/pagesPackage/TestScriptGIT.java
@@ -0,0 +1,33 @@
+package pagesPackage;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class TestScriptGIT {
+
+		By loginLink = By.xpath("//*[@id=\"pt-login\"]/a");
+		By userName= By.xpath("//*[@id=\"wpName1\"]");
+		By password = By.xpath("//*[@id=\"wpPassword1\"]");
+		By login = By.xpath("//*[@id=\"wpLoginAttempt\"]");
+		WebDriver driver;
+		
+		public  TestScriptGIT (WebDriver driver) {
+			this.driver = driver;
+		}
+		
+		public void clickLoginLink() {
+			driver.findElement(loginLink).click();
+		}
+		
+		public void typeUsername() {
+			driver.findElement(userName).sendKeys("Josephine Rathinam");
+		}
+		
+		public void typePassword() {
+			driver.findElement(password).sendKeys("maryjo23");
+		}
+		
+		public void loginButton() {
+			driver.findElement(login).click();
+	}
+}
